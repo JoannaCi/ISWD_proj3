e769455eb20dbd07448ede232a74cf9e9763aafe
maciejm2517 <maciej.konto.1357@gmail.com>
2024-04-28 22:27:59 +0200

    minor fixes

--- cicha_mak.py
+++ cicha_mak.py
@@ -1,10 +1,7 @@
-import numpy as np
 from player import Player
 
 class Nazwisko(Player):   
-    ### player's random strategy
     def putCard(self, declared_card):
-        ### check if must draw
         if len(self.cards) == 1 and declared_card is not None and self.cards[0][0] < declared_card[0]:
             return "draw"
         if declared_card is None and len(self.cards) >= 1:
@@ -12,7 +9,7 @@ class Nazwisko(Player):
         if declared_card is not None and len(self.cards) >= 1 and any(card[0] >= declared_card[0] for card in self.cards):
             return min((card[0], card[1]) for card in self.cards if card[0] >= declared_card[0]), min((card[0], card[1]) for card in self.cards if card[0] >= declared_card[0])
         if declared_card is not None and len(self.cards) >= 1 and not any(card[0] >= declared_card[0] for card in self.cards):
-            return 'draw'
+            return "draw"
     def checkCard(self, opponent_declaration):
         if opponent_declaration in self.cards:
             return True
